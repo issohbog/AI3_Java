c7dc3af77bc9b2a049859df87830d45058f1b264
tj-bu-706-23 <tj-bu-706-23@DESKTOP-AN7U6L8>
2025-03-14 09:41:23 +0900

    4일차

---
+++ Java/src/Day04/Ex03_Min.java
@@ -0,0 +1,54 @@
+package Day04;
+
+import java.util.Scanner;
+
+public class Ex03_Min {
+	public static void main(String[] args) {
+		// 첫번째 줄에 입력할 개수 N을 입력받고
+		// 둘째 줄에 N개의 정수를 공백을 두고 입력받으시오
+		// N개의 정수중 , 최솟값을 구하시오 
+		// (입력) 
+		// 5 
+		// 90 60 70 100 55
+		// (출력) 
+		// 최댓값 : 100 
+		
+		/*
+			(순서도) 
+			1. 정수 하나를 입력받아서 N에 대입. 
+			2. N개 요소를 갖는 배열 arr 선언 및 생성 
+			3. N번 반복하면서 정수를 입력받아 배열 arr에 순서대로 대입 
+			4. 배열을 반복해서 최솟값 min를 배열 i번째 요소랑 비교 
+			5. 두 요소중 더 작은 요소를 min에 대입
+			6. 반복이 끝나고, min를 출력 
+			
+		 */
+				
+		//1.
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		//2.
+		int arr[] = new int[N]; 
+		
+		
+		//3. 
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		//4. 
+		int min = Integer.MAX_VALUE; 
+		
+		for (int i = 0; i < arr.length; i++) {
+			if(min > arr[i]) {
+				//5. 
+				min = arr[i]; 
+			}
+		}
+		
+		//6.
+		System.out.println("최댓값 : " + min);
+		sc.close();
+	}
+}
---
+++ Java/src/Day04/Rainbow.java
@@ -0,0 +1,8 @@
+package Day04;
+
+// Rainbow 타입의 열거타입 생성 
+public enum Rainbow {
+	
+	// Rainbow 타입의 요소들
+	RED, ORANGE, YELLOW, GREEN, BLUE, NAVY, PURPLE
+}
